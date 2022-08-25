5750d05ae443c88ffa5dcc0733801a017ffbb70c
Yukiko Irliyani <73711768+yukikoirlyn@users.noreply.github.com>
2022-08-25 14:11:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -37,44 +37,57 @@ with st.sidebar:
     with col3:
         st.write("")    
 
-st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Efiensi Kebijakan: </div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center;font-size:40px;font-weight:bold;">Booster jadi Syarat Mobilitas</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:40px;font-weight:bold;line-height:0.5;">Efiensi Kebijakan: </div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:50px;font-weight:bold;line-height:1.5;">Booster jadi Syarat Mobilitas</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<div style="text-align:justify;font-size:15px;">Ditemukan bahwa peningkatan kasus Covid-19\
+st.markdown('<div style="text-align:center;font-size:20px;">Peningkatan kasus Covid-19\
      di beberapa negara terjadi begitu signifikan, seperti di Prancis, Italia, dan Jerman. Kenaikan\
         signifikan juga terjadi di negara tetangga, Singapura. Kabar baiknya, Indonesia menempati\
             posisi terendah pada kasus harian terhadap populasi, jika dibandingkan beberapa negara\
                 tetangga lainnya.\
-                    Vaksinasi booster efektif menjadi syarat perjalanan per 17 Juli 2022.\
-                        Syarat vaksin booster tersebut diatur dalam Surat Edaran No. 70 Tahun 2022 tentang petunjuk\
-                            Pelaksanaan Perjalanan orang Dalam Negeri (PPDN) dengan transportasi udara, darat, dan laut\
-                                pada masa pandemi Covid-2019.\
-                                    </div>', unsafe_allow_html=True)
+                    Di tengah peningkatan kasus yang terjadi, hal ini tentu sangat mengkhawatirkan, \
+                        mengingat antibodi masyarakat akan semakin berkurang.\
+                                </div>', unsafe_allow_html=True)
 st.markdown("---")
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Laporan Peduli Lindungi</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Laporan Peduli Lindungi</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
 col1, col2, col3 = st.columns([1,1,1])
 with col1:
-    st.markdown('<div style="text-align: center;font-size:15px;font-weight:bold;">Pengunjung Mall</div>', unsafe_allow_html=True)
-    st.image(image2,caption="source: freepik.com")
-with col2:
     st.markdown('<div style="text-align: center;font-size:70px;font-weight:bold;">1,9 jt</div>', unsafe_allow_html=True)  
-    st.markdown('<div style="text-align: center;font-size:40px;font-weight:bold;">orang</div>', unsafe_allow_html=True)
-    st.markdown('<div style="text-align: center;font-size:15px;font-weight:bold;">per hari</div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">orang</div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">per hari</div>', unsafe_allow_html=True)
+with col2:
+    st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Pergi ke Mall</div>', unsafe_allow_html=True)
+    st.image(image2,caption="source: freepik.com")
 with col3:
-    st.markdown('<div style="text-align: center;font-size:70px;font-weight:bold;">24,6 %</div>', unsafe_allow_html=True)  
-    st.markdown('<div style="text-align: center;font-size:40px;font-weight:bold;">booster</div>', unsafe_allow_html=True)                                   
+    st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Hanya</div>', unsafe_allow_html=True)
+    st.markdown('<div style="text-align: center;font-size:50px;font-weight:bold;">24,6 %</div>', unsafe_allow_html=True)  
+    st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">booster</div>', unsafe_allow_html=True)                                   
 st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<div style="text-align:justify;font-size:15px;">Wakil Direktur INDEF, Eko Listyanto menilai syarat \
-    vaksin booster untuk perjalanan dinilai tidak akan menggangu momentum pemulihan ekonomi yang \
-        sedang berjalan. Alasannya, saat ini kesadaran masyarakat untuk mendapatkan vaksin \
-            sudah terbangun dan cukup baik.\
+st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;line-height:2.5;">Kebijakan Pemerintah</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align:center;font-size:20px;">Vaksinasi booster efektif menjadi syarat perjalanan per 17 Juli 2022.\
+                Syarat vaksin booster tersebut diatur dalam Surat Edaran No. 70 Tahun 2022 tentang petunjuk\
+                    Pelaksanaan Perjalanan orang Dalam Negeri (PPDN) dengan transportasi udara, darat, dan laut\
+                        pada masa pandemi COVID-2019.\
                 </div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<div style="text-align:justify;font-size:40px;">Lalu?\
+st.markdown('<div style="text-align:center;font-size:20px;font-weight:bold;">"Syarat vaksin booster untuk perjalanan\
+    dinilai tidak akan mengganggu momentum\
+        pemulihan ekonomi yang sedang berjalan.\
+            Alasannya saat ini kesadaran masyarakat\
+                untuk mendapatkan vaksin sudah\
+                    terbangun dan cukup baik."\
                 </div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align:center;font-size:18px;">EKO LISTYANTO</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align:center;font-size:15px;">WAKIL DIREKTUR INDEF</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<div style="text-align:justify;font-size:15px;">Apakah kebijakan vaksin  booster tidak mempengaruhi mobilitas masyarakat?\
+st.markdown("---")
+col1, col2 = st.columns([1,4])
+with col1:
+    st.markdown('<div style="text-align:center;font-size:60px;">Lalu?\
+                </div>', unsafe_allow_html=True)
+with col2:
+    st.markdown('<div style="text-align:justify;font-size:20px;">Apakah kebijakan vaksin  booster tidak mempengaruhi mobilitas masyarakat?\
     Sedangkan jika dilogika setiap pertumbuhan mobilitas berarti dapat \
         mempengaruhi pertumbuhan ekonomi? Atau mungkinkah kesadaran masyarakat \
             saat ini sudah sangat tinggi untuk melakukan vaksin?\
@@ -84,7 +97,7 @@ st.markdown("---")
 
 st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Kebijakan Pemerintah vs Mobilitas Masyarakat</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Mobilitas Masyarakat Indonesia saat Pandemi</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:27px;">Mobilitas Masyarakat Indonesia saat Pandemi</div>', unsafe_allow_html=True)
 # dataset declaration
 df = pd.read_csv('mobility_change_202208012334.csv')
 
@@ -96,17 +109,15 @@ df['date'] = pd.to_datetime(df['date'])
 mobility = df[['date', 'retail_and_recreation', 
 'grocery_and_pharmacy', 'park', 'transit', 
 'workplace', 'resident']].set_index('date').resample('M').sum()
-
 st.line_chart(mobility)
-
-st.markdown('<div style="text-align: justify;font-size:15px;font-weight:bold;">Hal yang cukup menarik \
+st.markdown('<div style="text-align: justify;font-size:20px;font-weight:bold;">Hal yang cukup menarik \
     terasa pada dua kondisi:</div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: justifi;font-size:15px;">1. Pada saat kasus \
-    covid meningkat \
+st.markdown('<div style="text-align: justifi;font-size:20px;">1. Pada saat kasus \
+    covid meningkat\
         </div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: justify;font-size:15px;">2. Pada saat libur nasional. \
-        </div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: justify;font-size:15px;">Diketahui terdapat tiga gelombang \
+st.markdown('<div style="text-align: justify;font-size:20px;">2. Pada saat libur nasional. \
+    </div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: justify;font-size:20px;">Diketahui terdapat tiga gelombang \
     kasus covid yaitu yang pertama terjadi \
         hingga akhir Januari 2021, kedua pada saat adanya varian delta di sekitar pertengahan 2021,\
             serta yang ketiga pada akhir 2021 hingga awal 2022\
@@ -117,9 +128,8 @@ st.markdown('<div style="text-align: justify;font-size:15px;">Diketahui terdapat
 st.write("  ")
 st.write("")
 
-st.markdown("---")
 # heat map
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Heat Maps perubahan Mobilitas Masyarakat berdasarkan Sektornya</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:27px;">Heat Maps perubahan Mobilitas Masyarakat berdasarkan Sektornya</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
 select1, select2 = st.columns(2)
 with select1:
@@ -145,19 +155,22 @@ heat_m = px.imshow(dfp_p, color_continuous_scale='jet')
 heat_m.update_layout(height=500)
 st.plotly_chart(heat_m, use_container_width=True)
 
-st.markdown('<div style="text-align:justify;font-size:15px;">Data menjelaskan pada lokasi park \
-    di tahun 2022 memiliki sesuatu yang unik. Hal ini terlihat pada musim lebaran \
-        (Mei - Juni 2022) memiliki nilai mobilisasi yang cukup tinggi. Pada musim liburan saat itu, umumnya masyarakat \
-            lebih memilih ke tempat yang outdoor ketimbang harus ke dalam mall. Sehingga, \
-                kebijakan pemerintah disini tidak dapat berlaku.</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align:justify;font-size:20px;">Data menjelaskan di lokasi park \
+    semester awal tahun 2022 memiliki sesuatu yang unik terlihat pada musim lebaran, \
+        nilai mobilisasi masyarakat cukup tinggi. Pada musim liburan saat itu, umumnya masyarakat \
+            lebih memilih ke tempat yang outdoor ketimbang harus ke dalam mall.\
+                Sedangkan peraturan pemerintah berlaku di stasiun, bandara, maupun pelabuhan.\
+                    Sehingga, \
+                        kebijakan pemerintah disini membatasi masyarakat untuk melakukan mobilitas bisa\
+                            dikatakan tidak berlaku.</div>', unsafe_allow_html=True)
 
 st.write("")
 st.markdown("---")
 
-st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Correlation Analysist</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Analisis Korelasi</div>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
 
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Bagaimana Korelasi antara Mobilitas \
+st.markdown('<div style="text-align: center;font-size:27px;">Korelasi antara Mobilitas \
     Masyarakat dengan Produk Domestik Bruto</div>', unsafe_allow_html=True)
 # correlation mobility and GDP
 dfc = pd.read_csv('cor_pdb_mobility_202208031213.csv')
@@ -181,14 +194,14 @@ scatt.data[9].showlegend = True
 
 st.plotly_chart(scatt, use_container_width=True)
 
-st.markdown('<div style="text-align: justify;font-size:15px;">Melalui data di atas, \
+st.markdown('<div style="text-align: justify;font-size:20px;">Melalui data di atas, \
     dapat menunjukkan bahwa sangat erat kaitannya antara mobilitas\
-        masyarakat dengan pertumbuhan ekonomi. Selain itu, terdapat hal yang berbeda pada data di \
+        masyarakat dengan pertumbuhan ekonomi. Namun, terdapat hal yang unik pada data di \
             triwulan 3 2021 dan triwulan 1 2022. Keadaan yang menyimpang dapat disebabkan adanya masalah\
                 kesehatan/kenaikan kasus covid pada masa itu namun tidak membuat masyarakat hilang kreativitas\
                     untuk meningkatkan perekonomian.</div>', unsafe_allow_html=True)
-st.markdown("---")
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Produk Domestik Bruto \
+st.markdown('<br>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;font-size:27px;">Produk Domestik Bruto \
     berdasarkan Lapangan Usaha</div>', unsafe_allow_html=True)
 # cek sektor lapangan usaha
 dfl = pd.read_csv('pdb_lap_usaha.csv')
@@ -204,12 +217,11 @@ lap.update_layout(
     )
 st.plotly_chart(lap, use_container_width=True)
 
-st.markdown('<div style="text-align: justify;font-size:15px;">Kontribusi terbesar dalam perekonomian Indonesia terdapat pada sektor industri\
+st.markdown('<div style="text-align: justify;font-size:20px;">Kontribusi terbesar dalam perekonomian Indonesia terdapat pada sektor industri\
     pengolahan. Berdasarkan data tersebut, penyimpangan yang terjadi pada triwulan 4 2021 dan \
-        triwulan 1 2022 secara signifikan disebabkan oleh perubahan hasil usaha sektor pertanian,\
-            perhutanan, dan perikanan</div>', unsafe_allow_html=True)
+        triwulan 1 2022 secara signifikan disebabkan oleh jasa kesehatan dan kegiatan sosial.</div>', unsafe_allow_html=True)
 st.markdown("---")
-st.markdown('<div style="text-align: center;font-size:20px;font-weight:bold;">Korelasi antara Kenaikan \
+st.markdown('<div style="text-align: center;font-size:30px;font-weight:bold;">Korelasi antara Kenaikan \
     Vaksinasi dan Mobilitas Masyarakat</div>', unsafe_allow_html=True)
 # correlation between mobility and vaccination
 dfv = pd.read_csv('corr_vacc_mobility_202208031539.csv')
@@ -224,16 +236,16 @@ rsq_1 = results_1.iloc[0]["px_fit_results"].rsquared
 corr.data[4].name = 'y = ' + str(round(alpha_1, 2)) +' + ' + str(round(beta_1, 8)) + 'x' + ' | R-squared = ' + str(round(rsq_1, 3))
 corr.data[4].showlegend = True
 st.plotly_chart(corr, use_container_width=True)
-st.markdown('<div style="text-align: justify;font-size:15px;">Terdapat korelasi yang kuat antara total vaksinasi \
+st.markdown('<div style="text-align: justify;font-size:20px;">Terdapat korelasi yang kuat antara total vaksinasi \
     dengan nilai mobilitas\
         pada tahun 2020 - 2022 (r = 0,773). Hal ini menunjukkan bahwa kesadaran masyarakat terhadap\
             vaksinasi juga dipengaruhi oleh keinginan mereka untuk meningkatkan mobilitas\
     </div>', unsafe_allow_html=True)
 
 st.markdown("---")
-st.markdown('<div style="text-align:justify;font-size:40px;">Insight\
+st.markdown('<div style="text-align:justify;font-size:40px;">Recommendation\
                 </div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: justify;font-size:15px;">Varian Covid yang \
+st.markdown('<div style="text-align: justify;font-size:20px;">Varian Covid yang \
     semakin bermutasi memaksa pemerintah membentuk kebijakan baru untuk mendorong\
         masyarakat mendapatkan vaksin. Hal ini dijadikan syarat bagi siapa saja yang\
             menggunakan transportasi umum. Banyak pro dan kontra yang terjadi di masyarakat,\
@@ -242,7 +254,7 @@ st.markdown('<div style="text-align: justify;font-size:15px;">Varian Covid yang 
                         Berdasarkan data yang telah disajikan, wawasan yang dapat diberikan bahwa pemerintah \
                             harus lebih kreatif dalam memberikan kampanye karena kesadaran masyarakat saat ini \
                                 cukup baik untuk diberikan vaksin, meski mobilitas sangat berkorelasi dengan \
-                                    pertumbuhan ekonomi. Namun perlu diingat, bekerja melalui WFH juga dapat \
+                                    pertumbuhan ekonomi. Namun perlu diingat, bekerja melalui WFH (Work From Home) juga dapat \
                                         menumbuhkan perekonomian.\
                                              Kesadaran masyarakat untuk melakukan vaksin juga didukung oleh \
                                                 keinginan mereka untuk meningkatkan mobilitas, sehingga peran \
@@ -251,4 +263,7 @@ st.markdown('<div style="text-align: justify;font-size:15px;">Varian Covid yang 
 st.write("     ")
 
 st.markdown("---")
-st.write("Capstone Project, Tetris Program")
+st.markdown('<div style="text-align:center;font-size:17px;">Sumber Data:\
+    GOOGLE Community Mobility Report, Produk Domestik Bruto (PDB) menurut Lapangan Usaha by Badan\
+        Pusat Statistika (BPS), Total Vaccination by Our World in Data\
+                </div>', unsafe_allow_html=True)
